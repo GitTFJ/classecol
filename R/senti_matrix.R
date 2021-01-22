c2a73c9d57e7ddf79676912cb92a3dadd69413db
Thomas Frederick Johnson <thomas.frederick.johnson@outlook.com>
2021-01-22 10:29:21 +0000

    message

--- R/senti_matrix.R
+++ R/senti_matrix.R
@@ -1,6 +1,6 @@
 #' Create a sentiment matrix
 #'
-#' This function loads a vector or string and produces a sentiment matrix with 12 lexicon metrics: jockers-rinker, jockers, hu-liu, loughran-mcdonald, sentinet, sentiword, sentimentr, socal-google, nrc, bing, afinn, and meanr
+#' This function loads a vector or string and produces a sentiment matrix with 11 lexicon metrics: jockers-rinker, jockers, hu-liu, loughran-mcdonald, sentinet, sentiword, socal-google, nrc, bing, afinn, and meanr
 #' @param text_vector Choose character vector or string
 #' @return A trimmed character vector or string
 #' @examples
@@ -17,7 +17,6 @@ senti_matrix = function(text_vector){
     loughran_mcdonald = round(sentimentr::sentiment(text_vector, lexicon::hash_sentiment_loughran_mcdonald, question.weight = 0)[["sentiment"]], 2),
     senticnet = round(sentimentr::sentiment(text_vector, lexicon::hash_sentiment_senticnet, question.weight = 0)[["sentiment"]], 2),
     sentiword = round(sentimentr::sentiment(text_vector, lexicon::hash_sentiment_sentiword, question.weight = 0)[["sentiment"]], 2),
-    sentimentr = round(sentimentr::sentiment(text_vector)[["sentiment"]], 2),
     socal_google = round(sentimentr::sentiment(text_vector, lexicon::hash_sentiment_socal_google, question.weight = 0)[["sentiment"]], 2),
     nrc = syuzhet::get_sentiment(text_vector, method="nrc"),
     afinn = syuzhet::get_sentiment(text_vector, method="afinn"),
